9a59c7867e35e12f9254c202368dbe6708456cf3
PinguinPervert <klibragimov@edu.hse.ru>
2021-07-20 21:12:15 +0300

    List handlers added, tokens added, midleware added

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -7,9 +7,15 @@ import (
 
 type Authorization interface {
 	CreateUser(user todo.User) (int, error)
+	GetUser(username, password string) (todo.User, error)
 }
 
 type TodoList interface {
+	Create(userId int, list todo.TodoList) (int, error)
+	GetAll(userId int) ([]todo.TodoList, error)
+	GetById(userId, listId int) (todo.TodoList, error)
+	Delete(userId, listId int) error
+	Update(userId, listId int, input todo.UpdateListInput) error
 }
 
 type TodoItem interface {
@@ -24,5 +30,6 @@ type Repository struct {
 func NewRepository(db *sqlx.DB) Repository {
 	return Repository{
 		Authorization: NewAuthPostgres(db),
+		TodoList:      NewTodoListPostgres(db),
 	}
 }
